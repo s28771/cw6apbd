8b0913e337357a43c9e66de4d998a047cbc64c4e
Piotr Ryzak <s28771@pjwstk.edu.pl>
2024-04-16 11:45:40 +0200

    z lekcji

---
+++ CW6/CW6/Controllers/AnimalsController.cs
@@ -0,0 +1,53 @@
+﻿using CW6.Models;
+using CW6.Models.DTDs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace CW6.Controllers;
+
+[ApiController]
+//[Route("/animals")]
+[Route("api/[controller]")]
+public class AnimalsController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    public AnimalsController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        
+        SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+        SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "SELECT * FROM Animal;";
+
+        var reader = command.ExecuteReader();
+
+        List<Animal> animals = new List<Animal>();
+        int idAnimalOrdinal = reader.GetOrdinal("IdAnimal");
+        int nameOrdinal = reader.GetOrdinal("Name");
+        
+        while (reader.Read())
+        {
+            animals.Add(new Animal()
+            {
+                IdAnimal = reader.GetInt32(idAnimalOrdinal),
+                Name = reader.GetString(nameOrdinal)
+            });
+        }
+        return Ok(animals);
+    }
+
+    [HttpPost]
+    public IActionResult AddAnimal(AddAnimal animal)
+    {
+        
+        //z lekcji dorobic
+        return Created("", null);
+    }
+}
---
+++ CW6/CW6/Models/DTDs/AddAnimal.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CW6.Models.DTDs;
+
+public class AddAnimal
+{
+    [Required]
+    [MinLength(1)]
+    [MaxLength(200)]
+    public string Name { get; set; }
+    [MaxLength(200)]
+    public string? Description { get; set; }
+}
